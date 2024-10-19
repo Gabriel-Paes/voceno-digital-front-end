596d3498f6b7af31d59d443a3fbbb3d4e9690a76
Gabriel-Paes <paessouzagabriel@gmail.com>
2024-10-19 18:48:53 -0300

    feat: add hero

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,75 @@
 <template>
   <header>
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
+    <IconLogo color="#fff" width="55" height="55" />
+    <h2 class="ubuntu-medium">voceno.digital</h2>
+    <div class="spacer"></div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
   </header>
 
-  <RouterView />
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import IconLogo from '@/components/icons/IconLogo.vue'
 </script>
+
+<style lang="scss" scoped>
+@use './styles/main.scss' as *;
+
+$header-size: 70px;
+
+header {
+  background-color: $color-light-blue;
+  height: $header-size;
+  display: flex;
+  align-items: center;
+  padding: 2rem 4rem;
+
+  h2 {
+    color: $color-white;
+    font-size: 32px;
+    margin-left: 5px;
+    user-select: none;
+    -webkit-user-drag: none;
+  }
+
+  .spacer {
+    margin: auto;
+  }
+
+  nav {
+    a {
+      background-color: transparent;
+      color: $color-white;
+      border: thin solid $color-white;
+      padding: 8px 20px;
+      border-radius: 4px;
+      font-size: 16px;
+      cursor: pointer;
+      transition:
+        background-color 0.3s ease,
+        color 0.3s ease;
+      margin: 0 8px;
+
+      &:hover {
+        background-color: rgba(255, 255, 255, 0.2);
+      }
+      &:focus {
+        outline: none;
+        box-shadow: 0 0 5px rgba(74, 144, 226, 0.5);
+      }
+    }
+  }
+}
+
+main {
+  /* max-width: 1200px; */
+  margin: 0 6rem;
+  height: calc(100vh - $header-size);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,96 @@
 <template>
-  <main>
-    <h1>Home</h1>
-  </main>
+  <section id="hero">
+    <h1 class="ubuntu-medium">
+      Sua presença digital de forma <br />simples e rápida
+    </h1>
+    <h3>
+      Desenvolva sua presença digital com um template pronto.<br />
+      Sem códigos, sem complicações - apenas <span>você no digital</span>,
+      pronto para compartilhar!
+    </h3>
+    <div class="btn-group">
+      <a href="#" class="btn-primary">Comece agora</a>
+      <a href="#" class="btn-secondary">Saiba mais</a>
+    </div>
+  </section>
 </template>
+
+<style lang="scss" scoped>
+@use '../styles/main.scss' as *;
+
+#hero {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  gap: 80px;
+  text-align: center;
+
+  h1 {
+    font-size: 72px;
+  }
+  h3 {
+    font-size: 20px;
+
+    span {
+      background-color: $color-light-blue;
+      color: $color-white;
+
+      &::selection {
+        background-color: $color-white;
+        color: $color-light-blue;
+      }
+    }
+
+    br::selection {
+      background-color: $color-white;
+    }
+    &::selection {
+      background-color: $color-white;
+      color: $color-light-blue;
+    }
+  }
+  .btn-group {
+    display: flex;
+    gap: 20px;
+    .cta-buttons {
+      display: flex;
+      gap: 20px; /* Espaçamento entre os botões */
+    }
+
+    .btn-primary,
+    .btn-secondary {
+      padding: 15px 25px; /* Espaçamento interno */
+      border: none; /* Remove a borda padrão */
+      border-radius: 5px; /* Bordas arredondadas */
+      text-decoration: none; /* Remove o sublinhado */
+      font-weight: bold; /* Texto em negrito */
+      transition:
+        background-color 0.3s,
+        color 0.3s; /* Transição suave */
+      cursor: pointer; /* Indica que é clicável */
+    }
+
+    .btn-primary {
+      background-color: $color-light-blue; /* Azul para o botão primário */
+      color: #ffffff; /* Texto branco */
+    }
+
+    .btn-primary:hover,
+    .btn-primary:focus {
+      background-color: #0056b3; /* Azul mais escuro ao passar o mouse */
+    }
+
+    .btn-secondary {
+      background-color: #6c757d; /* Cinza para o botão secundário */
+      color: #ffffff; /* Texto branco */
+    }
+
+    .btn-secondary:hover,
+    .btn-secondary:focus {
+      background-color: #5a6268; /* Cinza mais escuro ao passar o mouse */
+    }
+  }
+}
+</style>
